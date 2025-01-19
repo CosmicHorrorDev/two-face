f3b92e847a2f7a1d1cd0622d870add402a706446
Cosmic Horror <CosmicHorrorDev@pm.me>
2025-01-18 22:21:06 -0700

    docs: Bump version to 0.4.1

--- src/lib.rs
+++ src/lib.rs
@@ -208,7 +208,7 @@ two-face = { version = ..., default-features = false, features = ["syntect-fancy
 /// ```
 /// assert_eq!(
 ///     two_face::acknowledgement_url(),
-///     "https://github.com/CosmicHorrorDev/two-face/blob/v0.4.0/generated/acknowledgements_full.md"
+///     "https://github.com/CosmicHorrorDev/two-face/blob/v0.4.1/generated/acknowledgements_full.md"
 /// );
 /// ```
 pub fn acknowledgement_url() -> &'static str {
