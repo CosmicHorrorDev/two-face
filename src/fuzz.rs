02e7c32c43f1bc209899f0cfffe79a12d4ffd5db
Cosmic Horror <CosmicHorrorDev@pm.me>
2025-02-04 17:56:41 -0700

    fix: Read fuzzer syntax set at runtime instead of compiletime

--- src/fuzz.rs
+++ src/fuzz.rs
@@ -1,6 +1,8 @@
+use std::fs;
+
 use syntect::{dumps, parsing::SyntaxSet};
 
 pub fn fuzzer_syntaxes() -> SyntaxSet {
-    let bytes = include_bytes!("../generated/fuzzer-syntaxes.bin");
-    dumps::from_uncompressed_data(bytes).unwrap()
+    let bytes = fs::read("generated/fuzzer-syntaxes.bin").unwrap();
+    dumps::from_uncompressed_data(&bytes).unwrap()
 }
