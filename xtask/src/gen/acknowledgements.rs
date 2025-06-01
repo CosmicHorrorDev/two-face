4a0e42e1e1e12dc250ed0f95f34b7f926b86b515
Cosmic Horror <CosmicHorrorDev@pm.me>
2025-05-31 20:25:11 -0600

    feat(xtask): Allow exempting bad license-like files

--- xtask/src/gen/acknowledgements.rs
+++ xtask/src/gen/acknowledgements.rs
@@ -2,11 +2,12 @@
 // but that seems excessive. Instead we can just add in the extra stuff used for generation here
 include!("../../../src/acknowledgement/core_types.rs");
 
-use std::{ffi::OsStr, fs, path::Path};
+use std::{collections::BTreeMap, ffi::OsStr, fs, path::Path};
 
 use super::utils;
 
 use anyhow::Context;
+use twox_hash::XxHash64;
 
 pub const NORM_LICENSE_STEMS: &[&str] = &["copying", "notice", "license"];
 const LICENSE_CACHE: &[u8] = include_bytes!("../../resources/spdx_cache.bin.zstd");
@@ -67,6 +68,22 @@ fn load_acknowledgements(
     base_dir: &Path,
     dir_name: &str,
 ) -> anyhow::Result<Vec<License>> {
+    // there's no standard to naming different licenses, so some may have license-like names while
+    // being some other form of (usually license related) notice
+    let exemptions: BTreeMap<PathBuf, u64> = [
+        // Idris2's NOTICE file just states their license policy in laymans terms in addition to
+        // their LICENSE file
+        (
+            Path::new("syntaxes")
+                .join("02_Extra")
+                .join("Idris2")
+                .join("NOTICE"),
+            0x432a39028e111f67,
+        ),
+    ]
+    .into_iter()
+    .collect();
+
     let mut licenses = Vec::new();
 
     // TODO: this loop is duped. Dedupe?
@@ -77,14 +94,30 @@ fn load_acknowledgements(
             .and_then(OsStr::to_str)
             .map(|stem| NORM_LICENSE_STEMS.contains(&&*stem.to_ascii_lowercase()))
             .unwrap_or(false);
-        if is_license {
-            let rel_path = file
-                .strip_prefix(base_dir)
-                .expect("Entry should always be prefixed by base dir");
-            let license = License::new(store, base_dir, rel_path)
-                .with_context(|| format!("Failed detecting license: {}", rel_path.display()))?;
-            licenses.push(license);
+        if !is_license {
+            continue;
         }
+
+        let rel_path = file
+            .strip_prefix(base_dir)
+            .expect("Entry should always be prefixed by base dir");
+
+        if let Some(&expected_hash) = exemptions.get(rel_path) {
+            let contents = fs::read(Path::new("bat").join("assets").join(rel_path)).unwrap();
+            let hash = XxHash64::oneshot(0xc0ffee, &contents);
+            assert_eq!(
+                expected_hash, hash,
+                "Exempted license-like file had a different hash than expected\nFound: 0x{hash:x}"
+            );
+
+            // exempt file
+            log::info!("Skipping exempt file {}", rel_path.display());
+            continue;
+        }
+
+        let license = License::new(store, base_dir, rel_path)
+            .with_context(|| format!("Failed detecting license: {}", rel_path.display()))?;
+        licenses.push(license);
     }
     // TODO: need to normalize to handle filesystems with different case sensitivity
 
